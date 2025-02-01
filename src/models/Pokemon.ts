d7f752463babe6518de9344e3b1517d029ed125c
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-02-01 22:02:03 +0100

    still need to make sure the scroll doesnt happen until after the show-component has loaded

--- src/models/Pokemon.ts
+++ src/models/Pokemon.ts
@@ -5,24 +5,24 @@ export type Pokemon = {
     stats: Stat[];
     types: Type[];
 
-}
+};
 
 type Stat = {
     base_stat: number;
 
-}
+};
 
 type Sprites = {
     front_default: string;
     back_default: string;
     
-}
+};
 
 type Type = {
     type: NameUrl;
-}
+};
 
 type NameUrl = {
     name: string;
    
-}
+};
