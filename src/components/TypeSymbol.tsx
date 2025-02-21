67f7788daecad76648404768c169af4057c0bb03
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-02-21 22:03:30 +0100

    all imgs and imports added

--- src/components/TypeSymbol.tsx
+++ src/components/TypeSymbol.tsx
@@ -1,6 +1,23 @@
 
 import { Pokemon } from "../models/Pokemon"
-
+import normal from "../assets/imgs/type-symbols/normal.png"
+import fire from "../assets/imgs/type-symbols/fire.png"
+import flying from "../assets/imgs/type-symbols/flying.png"
+import ghost from "../assets/imgs/type-symbols/ghost.png"
+import grass from "../assets/imgs/type-symbols/grass.png"
+import water from "../assets/imgs/type-symbols/water.png"
+import dragon from "../assets/imgs/type-symbols/dragon.png"
+import electric from "../assets/imgs/type-symbols/electric.png"
+import fighting from "../assets/imgs/type-symbols/fighting.png"
+import poison from "../assets/imgs/type-symbols/poison.png"
+import ground from "../assets/imgs/type-symbols/ground.png"
+import psychic from "../assets/imgs/type-symbols/psychic.png"
+import rock from "../assets/imgs/type-symbols/rock.png"
+import ice from "../assets/imgs/type-symbols/ice.png"
+import bug from "../assets/imgs/type-symbols/bug.png"
+import steel from "../assets/imgs/type-symbols/steel.png"
+import fairy from "../assets/imgs/type-symbols/fairy.png"
+import dark from "../assets/imgs/type-symbols/dark.png"
 type TypeSymbolProps = {
     pokemon: Pokemon
 }
