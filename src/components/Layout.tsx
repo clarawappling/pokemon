81af887fd2a8d1cc9fa4680f11129fc4fb2fa21c
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-02-21 22:58:42 +0100

    pokeball as favicon added

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,12 @@
 import { PokemonApp } from "./PokemonApp"
 import "../styles/Layout.css"
-import headerImg from "../imgs/pokemon-vector-logo.png"
-import footerImg from "../imgs/20161101_pokemon_mobile_2x_0e5e6082-2584-43d3-aba2-6f51082fee6a.webp"
 
 export const Layout = () => {
     return (<>
 
     <header>
         <div className="header-image-container">
-            <img src={headerImg} alt="pokemon logo"></img>
+            <img src="https://www.sketchappsources.com/resources/source-image/pokemon-vector-logo.png" alt="pokemon logo"></img>
         </div>
     </header>
     <main>
@@ -18,7 +16,7 @@ export const Layout = () => {
     <footer>
         <div className="footer">
             <div className="footer-img">
-                <img src={footerImg} alt="pokemon gathering"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6g2uyUTzImXttdEyHkvRk1dxatii_NMaQ-g&s" alt="pokemon gathering"/>
 
             </div>
         </div>
