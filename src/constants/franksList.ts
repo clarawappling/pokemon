f1fe0774eaedf68cb5cb65f0ce1d5a8d41aae384
CLARA WÄPPLING <clara.wappling@gmail.com>
2025-04-05 13:28:27 +0200

    started to sort

--- src/constants/franksList.ts
+++ src/constants/franksList.ts
@@ -1,26 +1,94 @@
 export  const franksList = ([
-    "scorbunny", "eternatus", "barraskewda", "grimmsnarl", 
-    "sylveon", "falinks", "haxorus", "growlithe", 
-    "grapploct", "gengar", "magikarp", "snorlax", "onix", "gyarados",
-    "steelix", "lapras", "cloyster", "butterfree", "machoke", "goldeen", "metapod", 
-    "seismitoad", "cinderace", "helioptile", "sudowoodo", "thievul", "rookidee", 
-    "gossifleur",  "purrloin", "zigzagoon", "raboot", "blipbug", "dottler",
-    "orbeetle", "caterpie", "grubbin", "vikavolt", "hoothoot", "corviknight",
-    "skwovet", "greedent", "pidove", "tranquill", "nickit", "obstagoon", "seedot",
-    "nuzleaf", "shiftry", "chewtle", "drednaw", "liepard", "yamper", "bunnelby", 
+    
+    // NORMAL
+    "eevee","sylveon", "jolteon",
+    "zigzagoon", "obstagoon",
+    "skwovet", "greedent",
+    "snorlax",
+    // ELD
+    "scorbunny", "raboot", "cinderace",
+    "growlithe", 
+    
+    // VATTEN
+    "barraskewda", 
+    "magikarp", "gyarados",
+    "cloyster",
+    "goldeen", "seaking",
+    "tympole", "palpitoad", "seismitoad",
+    "chewtle", "drednaw",
+    
+    "lapras",
+    // MÖRK
+    "grimmsnarl", 
+    "nickit","thievul",
+    "purrloin", "liepard",
+
+    // ELEKTRISK
+    "helioptile",
+
+    // GRÄS
+    "gossifleur",
+
+    // FE
+
+    // INSEKT
+    "caterpie", "metapod", "butterfree",
+    "blipbug", "dottler", "orbeetle",
+    "grubbin", "vikavolt",
+    "seedot", "nuzleaf", "shiftry",
+
+    //DRAKE
+    
+    "axew", "fraxure","haxorus",
+    //IS
+
+    //FIGHTING
+    "falinks",
+    "grapploct",
+    "machoke",
+
+    //MARK
+
+    //STEN
+    "onix", "steelix",
+    "bonsly", "sudowoodo",
+
+    //STÅL
+
+    //GIFT
+    "eternatus",
+
+    //FLYGANDE
+    "rookidee", "corviknight",
+    "hoothoot",
+    "pidove", "tranquill",
+
+    //SPÖKE
+    "gengar",
+
+    //PSYKISK
+    
+     
+    
+     
+        
+           
+         
+          
+           "yamper", "bunnelby", 
     "diggersby", "minccino", "oddish", "gloom","wingull", "pelipper","galvantula",
     "electrike", "vulpix", "delibird", "snorunt", "mudbray","dwebble", "golurk", "stufful",
     "snover", "wooper", "corphish", "ninjask", "tyrogue", "pancham", "bronzor", 
-    "stunky", "tympole", "palpitoad", "dusknoir", "seaking", "octillery", "wishiwashi-solo", 
+    "stunky",   "dusknoir",  "octillery", "wishiwashi-solo", 
     "wishiwashi-school", "pyukumuku", "sizzlipede", "rolycoly", "coalossal", "roggenrola",
     "timburr", "gurdurr", "woobat", "arrokuda", "meowth", "perrserker", "cutiefly",
     "pumpkaboo-average", "espurr", "swirlix", "chinchou", "lanturn", "scraggy",
     "stunfisk", "whiscash", "shellos", "barbaracle", "hattrem", "hatterene", "sawk",
-    "bonsly", "rhyhorn", "gothitelle", "cubchoo", "rufflet", "skorupi", "litwick", "sneasel",
-    "sigilyph", "torkoal", "sandaconda", "hawlucha", "flygon", "axew", "fraxure", "yamask",
+     "rhyhorn", "gothitelle", "cubchoo", "rufflet", "skorupi", "litwick", "sneasel",
+    "sigilyph", "torkoal", "sandaconda", "hawlucha", "flygon",  "yamask",
     "phantump", "togedemaru", "snom", "mantine", "wailmer", "bergmite", "toxel", "beartic",
-    "copperajah", "ditto", "frillish", "gastrodon", "glalie", "inkay", "jolteon", "kingler",
-    "klang", "ninetales", "noivern", "wobbuffet", "pikachu", "eevee", "avalugg", "glaceon", 
+    "copperajah", "ditto", "frillish", "gastrodon", "glalie", "inkay",  "kingler",
+    "klang", "ninetales", "noivern", "wobbuffet", "pikachu",  "avalugg", "glaceon", 
     "zacian", "shuckle", "gardevoir", "drakloak", "fuecoco", "lechonk", "tarountula", "hoppip",
     "fletchling", "pawmi", "yungoos", "scatterbug", "azurill", "buizel", "wooper", "psyduck",
     "ralts", "drowzee", "gastly", "fidough", "wingull", "wiglett", "flamigo", "crocalor", "charcadet",
