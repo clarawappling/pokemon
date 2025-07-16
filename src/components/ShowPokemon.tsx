0c84d2b32e55ac749b8d6a1588eaff97218acbec
Clara Wappling <clara.wappling@gmail.com>
2025-07-16 16:20:18 +0200

    updated name with replacing hyphens

--- src/components/ShowPokemon.tsx
+++ src/components/ShowPokemon.tsx
@@ -1,4 +1,4 @@
-import { Pokemon } from "../models/Pokemon"
+/* import { Pokemon } from "../models/Pokemon"
 import { TypeSymbol } from "./TypeSymbol";
 import "../styles/Layout.css"
 
@@ -33,4 +33,39 @@ export const ShowPokemon = (props: ShowPokemonProps) => {
         </div>
     </div>
     </>)
+} */
+
+    import { Pokemon } from "../models/Pokemon"
+import { TypeSymbol } from "./TypeSymbol";
+import "../styles/Layout.css"
+
+type ShowPokemonProps = {
+    pokemon: Pokemon;
 }
+
+export const ShowPokemon = (props: ShowPokemonProps) => {
+    
+    const showBack = (e: React.MouseEvent<HTMLImageElement>) => {
+        const target = e.target as HTMLImageElement;
+        if(props.pokemon.sprites.back_default) {
+            if (target.src === props.pokemon.sprites.front_default) {
+                target.src = props.pokemon.sprites.back_default;
+            } else {
+                target.src = props.pokemon.sprites.front_default;
+            }
+        }
+    };
+
+    const formattedName = props.pokemon.name.replace(/-/g, ' ');
+
+    return (
+        <div className="show-pokemon">
+            <h3 className="pokemon-name">{formattedName}</h3>
+            <div>
+                <img src={props.pokemon.sprites.front_default} onClick={showBack} />
+                <p className="hp">{props.pokemon.stats[0].base_stat}</p>
+                <TypeSymbol pokemon={props.pokemon} />
+            </div>
+        </div>
+    );
+};
